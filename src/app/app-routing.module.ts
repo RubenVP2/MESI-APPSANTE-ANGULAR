698a43c2d90efe33f37f8ce98d25ef381911e85f
MaxenceGlt <maxencegirault69@gmail.com>
2021-02-21 00:54:46 +0100

    ajout page html pour erreur 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component';
 import { IndexComponent } from './index/index.component';
@@ -7,11 +8,13 @@ import { UsersListeComponent } from './users-liste/users-liste.component';
 
 const routes: Routes = [
   //Après la virgule, utiliser canActivate: [AuthGuard]. ça permet de proteger votre url (l'utilisateur doit être connecté).
-  { path: '', component: IndexComponent},
-  { path: 'login', component: LoginComponent},
-  { path: 'users', component: UsersListeComponent},
-  { path: 'not-found', component: FourOhFourComponent}, //Path pour le component du not-found
-  { path: '**', redirectTo: '/not-found'} // Path qui permet de rediriger tous les urls inconnu par l'application sur l'url not-found
+  { path: '' , component: IndexComponent},
+  { path: 'login' , component: LoginComponent},
+  { path: 'users' , component: UsersListeComponent},
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+ // { path:'not-found', component: FourOhFourComponent}, //Path pour le component du not-found
+  //{ path:'**', redirectTo: '/not-found'} // Path qui permet de rediriger tous les urls inconnu par l'application sur l'url not-found
 ];
 
 @NgModule({
