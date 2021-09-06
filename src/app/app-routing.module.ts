2c88c36dabb343200adf0630b38fa6e914da8e6c
RubenVP2 <paulos.ruben@gmail.com>
2021-09-06 21:50:55 +0200

    New Feature Dashboard - Le Dashboard fonctionne avec du bootstrap 5 + librairie Chartist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { LoginComponent } from './login/login.component';
 import { UsersListeComponent } from './users-liste/users-liste.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent},
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'login', component: LoginComponent},
   { path: 'users', component: UsersListeComponent},
 ];
