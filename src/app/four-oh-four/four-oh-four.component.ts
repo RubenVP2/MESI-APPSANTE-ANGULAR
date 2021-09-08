9989f2223334cc5d01c94c06331e6007998ef3f4
MaxenceGlt <maxencegirault69@gmail.com>
2021-09-08 10:10:45 +0200

    Correction bug sur URL et creation d un component pour la page error404

---
+++ src/app/four-oh-four/four-oh-four.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-four-oh-four',
+  templateUrl: './four-oh-four.component.html',
+  styleUrls: ['./four-oh-four.component.css']
+})
+export class FourOhFourComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  retour(){
+    this.router.navigate(['/']);
+  }
+
+}
