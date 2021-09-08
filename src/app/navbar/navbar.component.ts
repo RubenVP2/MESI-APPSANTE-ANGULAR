2c477c4efef820f2c61c5b920ffa78ce442eedcd
RubenVP2 <paulos.ruben@gmail.com>
2021-09-08 16:56:34 +0200

    New Feature : - Ajout de la déconnexion

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/User.model';
 import { UserService } from '../services/user.service';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -12,7 +15,7 @@ export class NavbarComponent implements OnInit {
   user: User;
   username: string;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -28,5 +31,26 @@ export class NavbarComponent implements OnInit {
     );
     this.userService.emitUserSubject();
     return this.user.isAdmin;
+  }
+
+  deconnexion(): void {
+    Swal.fire({
+        title: 'Confirmation',
+        text: 'Êtes-vous sûr de vouloir vous déconnecter ?',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Déconnexion'
+    }
+    ).then((deconnexion) => {
+      if (deconnexion.value) {
+        sessionStorage.removeItem('user');
+        Swal.fire(
+          'Vous avez été déconnecté.',
+          'success'
+        ).then( () => {
+          window.location.reload();
+        });
+      }
+    });
   }
 }
